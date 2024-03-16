2db02fc11246235f960e36b699b435491719883f
SHUBH-17 <34864905+SHUBH-17@users.noreply.github.com>
2024-03-16 19:00:34 +0530

    Update json_mock_generator.py

--- json_mock_generator.py
+++ json_mock_generator.py
@@ -4,14 +4,17 @@ import random
 import datetime
 
 sqs_client = boto3.client('sqs')
-QUEUE_URL = ''  #add sqs queue url
+QUEUE_URL = 'https:......'  #sqs queue url
 
 def generate_booking_data(n):
     startDate = datetime.date(2024, 3, 1)
     if(n%2==0):
         endDate = startDate + datetime.timedelta(days=random.randint(2, 30))
     else:
-        endDate = startDate
+        endDate = startDate + datetime.timedelta(days=1)
+    
+    stayDuration = endDate - startDate
+    stayDuration = stayDuration.days
     
     # List of cities.
     cities = ['Gorakhpur','New York', 'London', 'Paris', 'Tokyo', 'Sydney']
@@ -28,6 +31,7 @@ def generate_booking_data(n):
         "location": location,
         "startDate": startDate,
         "endDate": endDate,
+        "stayDuration" : stayDuration,
         "price": round(random.uniform(100.0, 5000.0), 2)
     }
 	
@@ -36,10 +40,14 @@ def lambda_handler(event, context):
     while(i<=5):
         booking_data = generate_booking_data(i)
         print(json.dumps(booking_data, default=str))
+        
+        """
         sqs_client.send_message(
             QueueUrl=QUEUE_URL,
-            MessageBody=json.dumps(booking_data)
+            MessageBody=json.dumps(booking_data, default=str)
         )
+        """
+        
         i += 1
     
     return {
